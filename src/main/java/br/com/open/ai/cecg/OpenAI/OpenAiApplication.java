03ea2262ace8f41fcd86032b2fc8927de218a4ca
vinicius-marcos <marcos.antonio@ebanx.com.br>
2023-08-25 07:58:32 -0300

    Init

---
+++ src/main/java/br/com/open/ai/cecg/OpenAI/OpenAiApplication.java
@@ -0,0 +1,13 @@
+package br.com.open.ai.cecg.OpenAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/open/ai/cecg/OpenAI/OpenAiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.open.ai.cecg.OpenAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
